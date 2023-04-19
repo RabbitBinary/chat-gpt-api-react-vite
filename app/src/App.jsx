c3f7a519fa6f18c14fb98684a654e594ae7fa81c
RabbitBinary <vladimir.labat@gmail.com>
2023-04-19 14:26:42 +0200

    commit#2

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,7 +3,10 @@ import './App.css';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
 
+const API_KEY = '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';
+
 function App() {
+  const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([
     {
       message: "Hello, I am ChatGPT!",
@@ -24,8 +27,55 @@ function App() {
     // update our message state
     setMessages(newMessages);
 
-    // process message to chatGPT (send it over and see response)
+    // set a typing indicator (chatgpt is typing)
+    setTyping(true);
 
+    // process message to chatGPT (send it over and see response)
+    await processMessageToChatGPT(newMessages);
+  }
+
+  async function processMessageToChatGPT(chatMessages) {
+    // chatMessages { sender: "user" or "ChatGPT", message: "The message content here"}
+    // apiMessages { role: "user" or "assistant", content: "The message content here"}
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if(messageObject.sender === "ChatGPT") {
+        role="assistant"
+      } else {
+        role = "user"
+      }
+      return{ role: role, content: messageObject.message }
+    });
+
+    // role: "user" -> a message from the user, "assistant" -> a response from chatGPT
+    // "system" -> generally one initial message defining HOW we want chatGPT to talk
+
+    const systemMessage = {
+      role: "system",
+      content: "Explain all concepts like I am 10 years old."
+    }
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,
+        ...apiMessages
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+    });
   }
 
   return (
@@ -33,7 +83,9 @@ function App() {
       <div style={{ position: "relative", height: "550px", width: "700px" }}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
+            <MessageList
+              typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing" /> : null}
+            >
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />
               })}
